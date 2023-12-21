2857572c0cbd2dce34f82332406c678c8fae4ac0
Khushi Gupta <guptakhushi5555@gmail.com>
2023-12-21 22:42:20 +0530

    Top nav Logo and Search placed

--- src/components/Navbar/Nav.jsx
+++ src/components/Navbar/Nav.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import styles from "./Nav.module.scss";
+import NavList from "./NavList/NavList";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <div className={styles.nav}>
+      <div className={styles.nav__logo}>
+        <img src="/logo.jpg" alt="avataar_assignment_logo" />
+        <span>E-COMM</span>
+      </div>
+      <NavList />
+      <div className={styles.nav__search}>
+        <img
+          className={styles.nav__search_icon}
+          src="/search.png"
+          alt="search"
+        />
+        <input type="text" placeholder="Search something" />
+      </div>
+    </div>
+  );
 };
 
 export default Nav;
---
+++ src/components/Navbar/NavList/NavList.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./NavList.module.scss";
+
+const NavList = () => {
+  return <div className={styles.list_body}>NavList</div>;
+};
+
+export default NavList;
