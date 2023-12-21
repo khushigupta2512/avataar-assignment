d6f3286bbbf18aecb37a7f31f85ee06072c381e8
Khushi Gupta <guptakhushi5555@gmail.com>
2023-12-21 23:07:26 +0530

    Added support for very small viewsizes

--- src/components/Navbar/Nav.jsx
+++ src/components/Navbar/Nav.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import styles from "./Nav.module.scss";
 import NavList from "./NavList/NavList";
+import { useViewport } from "../../context/ResizeContext";
+import Ham from "./HamburgerMenu/Ham";
 
 const Nav = () => {
+  const { width } = useViewport();
+  const compact = 635;
+
+  if (width < compact) {
+    return (
+      <div className={styles.nav}>
+        <div className={styles.nav__logo}>
+          <img src="/logo.jpg" alt="avataar_assignment_logo" />
+          <span>E-COMM</span>
+        </div>
+        <Ham />
+      </div>
+    );
+  }
   return (
     <div className={styles.nav}>
       <div className={styles.nav__logo}>
